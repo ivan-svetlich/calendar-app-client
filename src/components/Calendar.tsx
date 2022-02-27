4f9012a41c7c1b16528435502a2ffb0d97ba863e
ivan.svetlich <ivansvetlich@gmail.com>
2022-02-27 09:37:29 -0300

    Renamed file

---
+++ src/components/Calendar.tsx
@@ -0,0 +1,95 @@
+import "./calendarStyles.css";
+import React, { useState } from "react";
+import useFetchItems from "../hooks/useFetchItems";
+import server from "../api/calendarServer";
+import { ItemDto } from "../types/Item";
+import WeekInterval from "../types/WeekInterval";
+import DatePicker from "./DatePicker";
+import CalendarEntries from "./CalendarEntries";
+import { useAppSelector } from "../store/hooks";
+import User from "../types/User";
+import Header from "./Header";
+
+const Calendar = () => {
+  const [week, setWeek] = useState<WeekInterval>();
+  const [itemsState, updateState] = useFetchItems(week);
+  const user: User | null = useAppSelector((state) => state.user.data);
+
+  const handleAddItem = (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>,
+    dueDate: Date,
+    inputs: string[],
+    setInputs: React.Dispatch<React.SetStateAction<string[]>>
+  ) => {
+    const weekday = Number.parseInt(e.currentTarget.id);
+    const description = inputs[weekday];
+    if (description) {
+      server
+        .addItem({ description, completed: false, removed: false, dueDate })
+        .then((response) => {
+          updateState(response.data, weekday);
+          setInputs((prev) =>
+            prev.map((content, index) => {
+              if (index === weekday) {
+                return "";
+              } else {
+                return content;
+              }
+            })
+          );
+        });
+    }
+  };
+
+  const handleMarkCompleted = (item: ItemDto, index: number) => {
+    const id = item.id;
+    const element = document.getElementById(`${id}-description`);
+
+    server
+      .updateItem(id, {
+        description: item.description,
+        completed: true,
+        removed: item.removed,
+        dueDate: item.dueDate,
+      })
+      .then((response) => {
+        updateState(response.data, index, true);
+        if (element) {
+          element.classList.add("completed");
+        }
+      });
+  };
+
+  const handleRemoveItem = (id: number) => {
+    const description = document.getElementById(`${id}-description`);
+    const removeBtn = document.getElementById(`${id}-remove`);
+    const completedBtn = document.getElementById(`${id}-completed`);
+    server.removeItem(id).then((response) => {
+      if (description) {
+        description.classList.add("removed");
+      }
+      if (removeBtn) {
+        removeBtn.classList.add("disabled");
+      }
+      if (completedBtn) {
+        completedBtn.classList.add("disabled");
+      }
+    });
+  };
+
+  return (
+    <div id="list-container">
+      <Header />
+      <DatePicker setWeek={setWeek} />
+      {itemsState.loading && "Loading..."}
+      <CalendarEntries
+        entries={itemsState.data}
+        handleAddItem={handleAddItem}
+        handleMarkCompleted={handleMarkCompleted}
+        handleRemoveItem={handleRemoveItem}
+      />
+    </div>
+  );
+};
+
+export default Calendar;
