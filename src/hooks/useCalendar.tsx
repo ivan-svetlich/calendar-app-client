3406ac1e0a8ef3b00ae14059f866125b2d46330e
ivan.svetlich <ivansvetlich@gmail.com>
2022-02-27 09:32:08 -0300

    Fixed behaviour

--- src/hooks/useCalendar.tsx
+++ src/hooks/useCalendar.tsx
@@ -1,36 +1,33 @@
 import { useEffect, useState } from "react";
 import { DateTime } from "luxon";
+import WeekInterval from "../types/WeekInterval";
 
 const useCalendar = (month: number, year: number) => {
-    const getWeeksOfMonth = (_month: number = DateTime.now().month as number, 
-        _year: number = DateTime.now().year) => { 
-        let start = DateTime.utc(_year, _month).startOf('month').startOf('week');
-        let end = DateTime.utc(_year, _month).startOf('month').endOf('week');
-        const weeksOfMonth = [];
+  const getWeeksOfMonth = (
+    _month: number = DateTime.now().month as number,
+    _year: number = DateTime.now().year
+  ) => {
+    let start = DateTime.utc(_year, _month).startOf("month").startOf("week");
+    let end = DateTime.utc(_year, _month).startOf("month").endOf("week");
+    const weeksOfMonth = [];
 
-        while(end.month === _month) {
-            weeksOfMonth.push(
-                {start: start,
-                end: end})
-            start = start.plus({days: 7})
-            end = end.plus({days: 7})
-        }
-        
-        return weeksOfMonth;
+    while (end.month === _month) {
+      weeksOfMonth.push({ start: start, end: end });
+      start = start.plus({ days: 7 });
+      end = end.plus({ days: 7 });
     }
 
-    type Week = {
-        start: DateTime,
-        end: DateTime
-    }
-    const [weeks, setWeeks] = useState<Week[]>(getWeeksOfMonth);
+    return weeksOfMonth;
+  };
 
-    useEffect(() => {
-        const weeksOfMonth = getWeeksOfMonth(month, year);
-        setWeeks([...weeksOfMonth]);
-    }, [month, year])
+  const [weeks, setWeeks] = useState<WeekInterval[]>(getWeeksOfMonth);
 
-    return weeks
-}
+  useEffect(() => {
+    const weeksOfMonth = getWeeksOfMonth(month, year);
+    setWeeks([...weeksOfMonth]);
+  }, [month, year]);
+
+  return weeks;
+};
 
 export default useCalendar;
--- src/hooks/useFetchItems.tsx
+++ src/hooks/useFetchItems.tsx
@@ -1,20 +1,25 @@
 import { DateTime } from "luxon";
-import { useEffect, useReducer, useState } from "react";
-import { ItemDto } from "../entities/Item";
-import { getItemsByInterval } from "../calendarServer";
+import { useEffect, useReducer } from "react";
+import { ItemDto } from "../types/Item";
+import server from "../api/calendarServer";
+import WeekInterval from "../types/WeekInterval";
+import { useNavigate } from "react-router-dom";
 
 export type ItemsState = {
-    loading: boolean,
-    data: Items[],
-    error: string | null
-}
+  loading: boolean;
+  data: Items[];
+  error: string | null;
+};
 
-type Items = {
-    dueDate: Date,
-    todoItems: ItemDto[]
-}
+export type Items = {
+  dueDate: Date;
+  todoItems: ItemDto[];
+};
 
-type ACTION = { type: "LOAD", payload: null } | { type: "SUCCESS"; payload: Items[] } | { type: "FAILURE", payload: string};
+type ACTION =
+  | { type: "LOAD"; payload: null }
+  | { type: "SUCCESS"; payload: Items[] }
+  | { type: "FAILURE"; payload: string };
 
 const initialState: ItemsState = { loading: false, data: [], error: null };
 
@@ -22,65 +27,70 @@ function fetchItemsReducer<ItemsState>(state: ItemsState, action: ACTION) {
   const { type, payload } = action;
 
   switch (type) {
-    case 'LOAD':
+    case "LOAD":
       return { ...state, loading: true, data: null, error: null };
-    case 'SUCCESS':
+    case "SUCCESS":
       return { ...state, loading: false, data: payload, error: null };
-    case 'FAILURE':
+    case "FAILURE":
       return { ...state, loading: false, data: null, error: payload };
     default:
       return state;
   }
+}
+
+export const useFetchItems = (
+  week: WeekInterval | undefined
+): [
+  ItemsState,
+  (itemDto: ItemDto, index: number, replace?: boolean) => void
+] => {
+  const [state, dispatch] = useReducer(fetchItemsReducer, initialState);
+  let navigate = useNavigate();
+
+  useEffect(() => {
+    if (week) {
+      fetchItems(week.start, week.end, dispatch);
+    }
+  }, [week]);
+
+  const updateState = (
+    itemDto: ItemDto,
+    index: number,
+    replace: boolean = false
+  ) => {
+    let updatedState = { ...(state as ItemsState) };
+    if (replace) {
+      updatedState.data[index].todoItems = updatedState.data[
+        index
+      ].todoItems.map((item) => {
+        if (item.id === itemDto.id) {
+          return itemDto;
+        } else {
+          return item;
+        }
+      });
+    } else {
+      updatedState.data[index].todoItems.push(itemDto);
+    }
+    dispatch({ type: "SUCCESS", payload: [...updatedState.data] });
+  };
+
+  return [state as ItemsState, updateState];
 };
 
-type Week = {
-    start: DateTime,
-    end: DateTime
-}
-
-export const useFetchItems = (weeks: Week[], index: number)
-    : [ItemsState, (itemDto: ItemDto, index: number, replace?: boolean) => void] => {
-    const [state, dispatch] = useReducer(fetchItemsReducer, initialState);
-
-    useEffect(() => {
-      if(weeks.length > index) {
-        fetchItems(weeks[index].start, weeks[index].end, dispatch);
-      }      
-    }, [index, weeks])
-
-    const updateState = (itemDto: ItemDto, index: number, replace: boolean = false) => {
-      let updatedState = {...(state as ItemsState)};
-      if(replace) {
-        updatedState.data[index].todoItems = updatedState.data[index].todoItems.map((item => {
-          if(item.id === itemDto.id) {
-            return itemDto;
-          }
-          else {
-            return item;
-          }
-        }))
-      }
-      else {
-        updatedState.data[index].todoItems.push(itemDto);
-      } 
-      dispatch({ type: 'SUCCESS', payload: [...updatedState.data] });
-    }
-
-    return [state as ItemsState, updateState];
-}
-
-async function fetchItems(startDate: DateTime, endDate: DateTime, dispatch: React.Dispatch<ACTION>) {
-    dispatch({ type: 'LOAD', payload: null });
-    try {
-        await getItemsByInterval(startDate, endDate)
-            .then(response => {
-                dispatch({ type: 'SUCCESS', payload: response.data });
-            }); 
-    }
-    catch (error: any) {
-        dispatch({ type: 'FAILURE', payload: error });
-    }
-
+async function fetchItems(
+  startDate: DateTime,
+  endDate: DateTime,
+  dispatch: React.Dispatch<ACTION>
+) {
+  dispatch({ type: "LOAD", payload: null });
+  try {
+    await server.getItemsByInterval(startDate, endDate).then((response) => {
+      dispatch({ type: "SUCCESS", payload: response.data });
+    });
+  } catch (error: any) {
+    dispatch({ type: "FAILURE", payload: error });
+  }
 }
 
 export default useFetchItems;
