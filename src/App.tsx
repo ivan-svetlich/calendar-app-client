25f26d1ac17872ae3dc418d3d47663e240934d98
ivan.svetlich <ivansvetlich@gmail.com>
2022-02-27 09:40:07 -0300

    Run prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,35 @@
-import React from 'react';
-import './App.css';
-import List from './components/List';
+import React from "react";
+import { Provider } from "react-redux";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { persistStore } from "redux-persist";
+import { PersistGate } from "redux-persist/integration/react";
+import "./App.css";
+import AppNavBar from "./components/AppNavbar";
+import Calendar from "./components/Calendar";
+import Login from "./components/Login";
+import Signup from "./components/Signup";
+import { store } from "./store/store";
+
+let persistor = persistStore(store);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">       
-      </header>
-      <div className="App-body">
-        <List />
-      </div>
-    </div>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <Router>
+          <div className="App">
+            <div className="App-body">
+              <AppNavBar />
+              <Routes>
+                <Route path="/calendar" element={<Calendar />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/signup" element={<Signup />} />
+              </Routes>
+            </div>
+          </div>
+        </Router>
+      </PersistGate>
+    </Provider>
   );
 }
 
