9ea58f034c07c4fe05e792f16e67001d0a8ea2b6
ivan.svetlich <ivansvetlich@gmail.com>
2022-02-27 09:30:04 -0300

    Added types

---
+++ src/types/User.ts
@@ -0,0 +1,12 @@
+type User = {
+  username: string;
+  email: string;
+};
+
+export type UserDto = {
+  username: string;
+  email: string;
+  token: string;
+};
+
+export default User;
---
+++ src/types/WeekInterval.ts
@@ -0,0 +1,8 @@
+import { DateTime } from "luxon";
+
+type WeekInterval = {
+  start: DateTime;
+  end: DateTime;
+};
+
+export default WeekInterval;
